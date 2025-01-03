a941aea8324569592f23a92a06e8320f23e4282d
baijian <baijian@xingwenzn.com>
2025-01-03 13:43:32 +0800

    init

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,1 +1,1 @@
-sed -i 's#{ENV_PORT}#'"${PORT}"'#g; s#{HEROKU_HOME}#'"${PWD}"'#g' deploy/nginx.conf && heroku-php-nginx -c deploy/nginx.conf
+chmod +x deploy/nginx.conf && sed -i 's#{ENV_PORT}#'"${PORT}"'#g; s#{HEROKU_HOME}#'"${PWD}"'#g' deploy/nginx.conf && heroku-php-nginx -c deploy/nginx.conf
